35c7f170ad0eadbaeeb260e85a41e5b68b67e3c3
SnowPhoenix <snowphoenix347@gmail.com>
2019-12-18 21:35:32 +0300

    Added basics sprites and player control logic

---
+++ Assets/Scripts/GroundChecker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour
+{
+    private BoxCollider2D _boxCollider2D;
+
+    private void Start()
+    {
+        _boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    public bool IsGrounded(LayerMask groundLayerMask)
+    {
+        RaycastHit2D rayCastHit2D = Physics2D.BoxCast(_boxCollider2D.bounds.center, _boxCollider2D.bounds.size, 0f, Vector2.down, 1f, groundLayerMask);
+
+        return rayCastHit2D.collider != null;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpForce;
+    [SerializeField] private LayerMask _groundLayerMask;
+    private Rigidbody2D _playerRigidBody2D;
+    private GroundChecker _groundChecker;
+
+    private void Start()
+    {
+        _playerRigidBody2D = transform.GetComponent<Rigidbody2D>();
+        _groundChecker = GetComponent<GroundChecker>();
+    }
+    private void Update()
+    {
+        _playerRigidBody2D.velocity = new Vector2(_speed, _playerRigidBody2D.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space) && _groundChecker.IsGrounded(_groundLayerMask))
+        {
+            _playerRigidBody2D.velocity = Vector2.up * _jumpForce;
+        }
+    }
+}
