5a8a218669b07fb8b5fef52029eb1d6886cc42a2
SnowPhoenix <snowphoenix347@gmail.com>
2019-12-25 23:27:49 +0300

    Added Coins logic and Menu

---
+++ Assets/Scripts/CoinsCollector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinsCollector : MonoBehaviour
+{
+    [SerializeField] private Text _coinCountText;
+    private int _coinsCount = 0;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Coin")
+        {
+            AddCoin();
+            ShowCoinsCount(_coinsCount);
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void AddCoin()
+    {
+        _coinsCount++;
+    }
+
+    private void ShowCoinsCount(int coins)
+    {
+        _coinCountText.text = coins.ToString();
+    }
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private Animator _creatorsAnimator;
+    public void OnPlayPress()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnCreatorsPress()
+    {
+        _creatorsAnimator.SetBool("IsShow", !_creatorsAnimator.GetBool("IsShow"));
+    }
+
+    public void OnExitPress()
+    {
+        Application.Quit();
+    }
+}
